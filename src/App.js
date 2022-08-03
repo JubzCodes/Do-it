86fecb4fbb0a865f021de7ca291a5f63d957779f
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-08-03 18:28:23 -0400

    set up states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  //INPUT STATES
+   const [task, setTask] = useState('')
+   const [updateTask, setUpdateTask] = useState('')
+
+
+  //DO IT DEFAULT STATES
+ const [doIt, setDoIt] = useState([
+   { id: 1, label: "Build an App", status: false },
+   { id: 2, label: "Go for a Run", status: false }
+ ]);
+
+
+
+
+
+
   return (
     <div className="app">
       <h1 className='title'>Just Do It List</h1>
